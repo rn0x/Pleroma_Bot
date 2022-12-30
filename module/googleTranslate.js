dc8d250bb27e4a25ffb06f9c35875b1acedd42cc
rn0x <76129163+rn0x@users.noreply.github.com>
2022-12-30 17:16:37 +0300

    Update googleTranslate.js

--- module/googleTranslate.js
+++ module/googleTranslate.js
@@ -1,5 +1,4 @@
-
-import puppeteer from 'puppeteer';
+import { launch } from 'puppeteer';
 import fs from 'fs-extra';
 import path from 'path';
 
@@ -7,28 +6,30 @@ export default async function googleTranslate(text) {
 
     try {
 
-        const __dirname = path.resolve();
+        let __dirname = path.resolve();
         let config = fs.readJSONSync(path.join(__dirname, './config.json'));
         let launchOptions = {
             headless: true,
             args: ['--start-maximized', '--no-sandbox', '--disable-setuid-sandbox'],
             executablePath: config?.executablePath
         };
-        let browser = await puppeteer.launch(launchOptions);
-        let page = await browser.newPage();
-        page.setDefaultNavigationTimeout(0);
+        let browser = await launch(launchOptions).catch(e => console.log('Error: browser is not launch ', e));
+        let page = await browser?.newPage();
+        page?.setDefaultNavigationTimeout(600000);
 
         // https://translate.google.com/#view=home&op=translate&sl=auto&tl=ar&text=${text}
 
-        await page.goto(`https://translate.google.com/?hl=ar&sl=auto&tl=ar&text=${text}`, {
+        await page?.goto(`https://translate.google.com/?hl=ar&sl=auto&tl=ar&text=${text}`, {
             waitUntil: 'load',
-            timeout: 0
+            timeout: 600000
         });
         let XPath = '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div/div[8]/div/div[1]/span[1]'
-        await page.waitForXPath(XPath);
-        let [element] = await page.$x(XPath);
-        let translate = await page.evaluate(e => e?.innerText, element);
-        await browser.close();
+        await page?.waitForXPath(XPath);
+        let [element] = await page?.$x(XPath);
+        let translate = await page?.evaluate(e => e?.innerText, element);
+        await page?.setCacheEnabled(true);
+        await browser?.close();
+
         return translate
 
     } catch (error) {
