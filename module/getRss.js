524e63554bee52334ae88502ffbed93ab72a823c
rn0x <76129163+rn0x@users.noreply.github.com>
2022-12-30 17:16:01 +0300

    Update getRss.js

--- module/getRss.js
+++ module/getRss.js
@@ -35,16 +35,19 @@ export default async function getRss(callback) {
 
         for (let item of config?.rss) {
 
-            let feed = await new Parser({ timed: 0 }).parseURL(item).catch(e => console.log(`timed out ${item}`));
-            if (feed?.items?.length !== 0 && sentRss?.includes(feed?.items?.[0]?.link) === false && feed?.items?.[0]?.link) {
+            let feed = await new Parser({ timed: 600000 }).parseURL(item).catch(e => console.log(`timed out ${item}`));
+            let content = feed?.items[0]?.contentSnippet?.replace(/\n\n/g, " ")?.slice(0, 350);
+            let title = feed?.items?.[0]?.title;
+            let link = feed?.items?.[0]?.link
 
-                let content = feed?.items[0]?.contentSnippet?.replace(/\n\n/g, " ")?.slice(0, 350);
-                let title_ar = feed?.language?.includes('ar') ? feed?.items?.[0]?.title : await googleTranslate(feed?.items?.[0]?.title).catch((e) => console.log(e));
+            if (feed?.items?.length !== 0 && sentRss?.includes(link) === false && title && title !== '' && content && content !== '') {
+
+                let title_ar = feed?.language?.includes('ar') ? title : await googleTranslate(title).catch((e) => console.log(e));
                 let content_ar = feed?.language?.includes('ar') ? content : await googleTranslate(content).catch((e) => console.log(e));
 
                 if (title_ar && content_ar) {
 
-                    sentRss.push(feed?.items?.[0]?.link);
+                    sentRss.push(link);
                     fs.writeJSONSync(path.join(__dirname, './database/sentRss.json'), sentRss, { spaces: '\t' });
                     callback({
                         website: item,
@@ -54,9 +57,9 @@ export default async function getRss(callback) {
                         title: title_ar,
                         content: content_ar,
                         creator: feed?.items?.[0]?.creator,
-                        link: feed?.items?.[0]?.link,
+                        link: link,
                         isoDate: feed?.items?.[0]?.isoDate,
-                        image: await getImageUrl(feed?.items?.[0]?.content, feed?.items[0]?.link).catch(e => console.log(e))
+                        image: await getImageUrl(feed?.items?.[0]?.content, link).catch(e => console.log(e))
                     })
 
                 }
