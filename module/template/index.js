4a287f8c0e1ad082c6c880d3d27b2eb3751fca31
rn0x <76129163+rn0x@users.noreply.github.com>
2022-12-30 17:15:08 +0300

    Update index.js

--- module/template/index.js
+++ module/template/index.js
@@ -13,23 +13,23 @@ export default async function template(filename, titel, content, image, icon, ur
             image: image,
             icon: icon,
             url: url
-        
         });
-        const __dirname = path.resolve();
+        let __dirname = path.resolve();
         let config = fs.readJSONSync(path.join(__dirname, './config.json'));
         let launchOptions = {
             headless: true,
             args: ['--start-maximized', '--no-sandbox', '--disable-setuid-sandbox'],
             executablePath: config?.executablePath
         };
-        let browser = await launch(launchOptions);
-        let page = await browser.newPage();
+        let browser = await launch(launchOptions).catch(e => console.log('Error: browser is not launch ', e));
+        let page = await browser?.newPage();
         await page?.setViewport({ width: 700, height: 0 });
         await page?.goto(`file://${path.join(__dirname, './module/template/index.html')}`, {
             waitUntil: 'load',
-            timeout: 0
+            timeout: 600000
         });
         await page?.screenshot({ path: filename, fullPage: true });
+        await page?.setCacheEnabled(true);
         await browser?.close();
 
         return {
